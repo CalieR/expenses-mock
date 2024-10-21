f7ba0c884256fb4796754b8411e0a1bbca853ffb
Calie Rushton <calielrushton@gmail.com>
2024-10-21 16:51:01 +0100

    Added some filter choosers

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -12,24 +12,94 @@ const Table = (props: TableProps) => {
   const [tableRows, setTableRows] = useState(rows);
   const [sortAscending, setSortAscending] = useState<boolean>(true);
 
+  const [filterColumn, setFilterColumn] = useState('');
+  const [filterCondition, setFilterCondition] = useState('');
+  const [filterValue, setFilterValue] = useState('');
+
   const handleSortByColumn = (sortColumn: string) => {
     setSortAscending(!sortAscending);
     setTableRows(tableRows.sort(sortByColumn(sortColumn, sortAscending)));
   };
 
   const handleFilter = () => {
-    const filteredRows = filterItemsBy(tableRows, 'merchant', 'W', 'contains');
+    const filteredRows = filterItemsBy(
+      tableRows,
+      filterColumn,
+      filterValue,
+      filterCondition
+    );
     setTableRows(filteredRows);
   };
 
   const resetFilters = () => {
+    setFilterColumn('');
+    setFilterCondition('');
+    setFilterValue('');
     setTableRows(tableRowsCache);
   };
 
   return (
     <>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-4" onClick={handleFilter}>Filter by merchant starts with W</button>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={resetFilters}>Reset filters</button>
+      <div className="flex items-end gap-4">
+        <label className=" w-full max-w-x">
+          <div className="label">
+            <span className="label-text">Filter column:</span>
+          </div>
+          <select
+            id="filter-column-select"
+            className="bg-opacity-100"
+            value={filterColumn}
+            onChange={(e) => setFilterColumn(e.target.value)}
+          >
+            <option value="">Select a column</option>
+            {columns.map((column) => (
+              <option key={column.accessor} value={column.accessor}>
+                {column.header}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label className=" w-full max-w-x">
+          <div className="label">
+            <span className="label-text">Filter condition:</span>
+          </div>
+          <select
+            id="filter-condition-select"
+            className="select select-bordered"
+            value={filterCondition}
+            onChange={(e) => setFilterCondition(e.target.value)}
+          >
+            <option value="">Select a condition</option>
+            <option value="equals">Equals</option>
+            <option value="contains">Contains</option>
+          </select>
+        </label>
+        <label className="w-full max-w-x">
+          <div className="label">
+            <span className="label-text">Filter value:</span>
+          </div>
+          <input
+            id="filter-value-input"
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered input-md w-full max-w-xs"
+            value={filterValue}
+            onChange={(e) => setFilterValue(e.target.value)}
+          />
+        </label>
+      </div>
+      <button
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-4"
+        onClick={handleFilter}
+      >
+        Apply Filters
+      </button>
+      <button
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        onClick={resetFilters}
+      >
+        Reset filters
+      </button>
       <div data-testid="table">
         <table className="table-auto">
           <thead className="min-w-full">
